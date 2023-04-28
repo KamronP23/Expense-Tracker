88c5e7b0e5d062f4f19d168d819b181fbf7e6fe8
Kamron <kamronpoosti@yahoo.com>
2023-04-28 09:37:53 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+
+function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021 ,0, 28)
+    },
+    {
+      id: 2,
+      title: "Car Payment",
+      amount: 228.00,
+      date: new Date(2021 ,1, 28)
+    },
+    {
+      id: 3,
+      title: "Gas Payment",
+      amount: 41.75,
+      date: new Date(2021 ,3, 28)
+    },
+    {
+      id: 4,
+      title: "Carwash Cost",
+      amount: 24.99,
+      date: new Date(2021 ,4, 28)
+    },
+  ]
+
+  return (
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
